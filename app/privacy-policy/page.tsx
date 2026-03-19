17ff28b9654540c19d6243393e171764080cad60
leeseulahn <118793632+leeseulahn@users.noreply.github.com>
2026-03-19 12:28:58 +0900

    Update page.tsx

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Privacy Policy",
   description: "Privacy Policy for Textify Lab",
 };
