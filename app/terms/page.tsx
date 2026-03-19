1044920af9ce4b0a204e5bd2a6914375422f448b
leeseulahn <118793632+leeseulahn@users.noreply.github.com>
2026-03-19 12:29:06 +0900

    Update page.tsx

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Terms of Service",
   description: "Terms of Service for Textify Lab",
 };
